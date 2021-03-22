9a42c4d174e6f193a283448d7cc3b08ce3cf40a4
Andrey Skobelev <v1212186@gmail.com>
2021-03-22 22:13:52 +0300

    test: argument null exception tests for EncryptedFileProvider

--- Assets/Tests/EncryptedFileProviderTests.cs
+++ Assets/Tests/EncryptedFileProviderTests.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using LocalStorage.Encryption;
 using LocalStorage.Providers;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace LocalStorage.Tests
@@ -12,6 +15,13 @@ namespace LocalStorage.Tests
 
         protected override IFileProvider FileProvider => _encryptedFileProvider;
 
+        private static IEnumerable<object[]> _argumentNullExceptionCases()
+        {
+            yield return new object[] {null, null};
+            yield return new object[] {Substitute.For<IFileProvider>(), null};
+            yield return new object[] {null, Substitute.For<IEncryptionSettings>()};
+        }
+
         protected override byte[] GetExpectedResultFor(byte[] data)
         {
             return data.Encrypt();
@@ -20,6 +30,16 @@ namespace LocalStorage.Tests
         protected override byte[] PrepareDataForRead(byte[] data)
         {
             return data.Encrypt();
+        }
+
+        [TestCaseSource(nameof(_argumentNullExceptionCases))]
+        public void EncryptedFileProvider_ThrowsArgumentNullException(IFileProvider fileProvider,
+            IEncryptionSettings encryptionSettings)
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var provider = new EncryptedFileProvider(fileProvider, encryptionSettings);
+            });
         }
     }
 }
