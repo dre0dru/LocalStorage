5731d50955b10ee450e060a70562fa78d9de0576
Andrey Skobelev <dre0druofficial@gmail.com>
2021-03-31 23:38:41 +0300

    feat: added generic storage
    feat: added async api for file reading/writing

--- Runtime/Encryption/EncryptedFileProvider.cs
+++ Runtime/Encryption/EncryptedFileProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine.Scripting;
 
 namespace LocalStorage.Encryption
@@ -12,23 +13,47 @@ namespace LocalStorage.Encryption
         public EncryptedFileProvider(IFileProvider fileProvider,
             IEncryptionSettings encryptionSettings)
         {
-            _fileProvider = fileProvider  ??
-                            throw new ArgumentNullException(nameof(fileProvider));;
-            _encryptionSettings = encryptionSettings  ??
-                                  throw new ArgumentNullException(nameof(encryptionSettings));;
+            _fileProvider = fileProvider ??
+                            throw new ArgumentNullException(nameof(fileProvider));
+            ;
+            _encryptionSettings = encryptionSettings ??
+                                  throw new ArgumentNullException(nameof(encryptionSettings));
+            ;
         }
 
         public void Write(byte[] output, string filePath)
         {
-            var encrypted = AesEncryption.Encrypt(output, _encryptionSettings.Key,
-                _encryptionSettings.InitializationVector);
+            var encrypted = Encrypt(output);
             _fileProvider.Write(encrypted, filePath);
+        }
+
+        public Task WriteAsync(byte[] output, string filePath)
+        {
+            var encrypted = Encrypt(output);
+            return _fileProvider.WriteAsync(encrypted, filePath);
+        }
+
+        private byte[] Encrypt(byte[] data)
+        {
+            return AesEncryption.Encrypt(data, _encryptionSettings.Key,
+                _encryptionSettings.InitializationVector);
         }
 
         public byte[] Read(string filePath)
         {
             var bytes = _fileProvider.Read(filePath);
-            return AesEncryption.Decrypt(bytes, _encryptionSettings.Key,
+            return Decrypt(bytes);
+        }
+
+        public async Task<byte[]> ReadAsync(string filePath)
+        {
+            var bytes = await _fileProvider.ReadAsync(filePath);
+            return Decrypt(bytes);
+        }
+
+        private byte[] Decrypt(byte[] data)
+        {
+            return AesEncryption.Decrypt(data, _encryptionSettings.Key,
                 _encryptionSettings.InitializationVector);
         }
     }
--- Runtime/Providers/FileProvider.cs
+++ Runtime/Providers/FileProvider.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading.Tasks;
 
 namespace LocalStorage.Providers
 {
@@ -9,9 +10,36 @@ namespace LocalStorage.Providers
             File.WriteAllBytes(filePath, output);
         }
 
+        public async Task WriteAsync(byte[] output, string filePath)
+        {
+            using var fileStream =
+                new FileStream(
+                    filePath,
+                    FileMode.Create, FileAccess.Write, FileShare.None,
+                    4096, true);
+
+            await fileStream.WriteAsync(output, 0, output.Length);
+        }
+
         public byte[] Read(string filePath)
         {
             return File.ReadAllBytes(filePath);
+        }
+
+        public async Task<byte[]> ReadAsync(string filePath)
+        {
+            using var fileStream =
+                new FileStream(
+                    filePath,
+                    FileMode.Open, FileAccess.Read, FileShare.Read,
+                    4096, true);
+
+
+            var buffer = new byte[fileStream.Length];
+            //Assuming data length is < int.MaxValue
+            await fileStream.ReadAsync(buffer, 0, buffer.Length);
+
+            return buffer;
         }
     }
 }
