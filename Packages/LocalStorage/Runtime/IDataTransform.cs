bd3ed83dbc2b4846fd30203d83cf46b76e3b8ded
Andrey Skobelev <dre0druofficial@gmail.com>
2021-05-21 16:05:42 +0300

    feat: added optional `UniTask` support for async API

--- Packages/LocalStorage/Runtime/IDataTransform.cs
+++ Packages/LocalStorage/Runtime/IDataTransform.cs
@@ -1,15 +1,27 @@
+#if !DISABLE_UNITASK_SUPPORT && UNITASK_SUPPORT
+using Cysharp.Threading.Tasks;
+#else
 using System.Threading.Tasks;
+#endif
 
 namespace LocalStorage
 {
     public interface IDataTransform
     {
         byte[] Apply(byte[] data);
-        
+
+        #if !DISABLE_UNITASK_SUPPORT && UNITASK_SUPPORT
+        UniTask<byte[]> ApplyAsync(byte[] data);
+        #else
         Task<byte[]> ApplyAsync(byte[] data);
+        #endif
 
         byte[] Reverse(byte[] data);
-        
+
+        #if !DISABLE_UNITASK_SUPPORT && UNITASK_SUPPORT
+        UniTask<byte[]> ReverseAsync(byte[] data);
+        #else
         Task<byte[]> ReverseAsync(byte[] data);
+        #endif
     }
 }
